1eac9ba0d1770c09aa549ebed0e0bc463022adbb
clemenceroumy <clemenceroumy@gmail.com>
2026-08-18 14:50:41 +0200

    feat: apply colors on theme

--- lib/core/presentation/theme/app_colors.dart
+++ lib/core/presentation/theme/app_colors.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const seedColor = Color(0xFF4F583B);
+  static const primary = Color(0xFF4F583B);
+  static const secondary = Color(0xFFF4B840);
+  static const secondaryDisabled = Color(0xFFe7dfcd);
+  static const tertiary = Color(0xFF964526);
+  static const surface = Color(0xFFF8F4E8);
+  static const background = Color(0xFFEAD7B2);
+  static const foreground = Color(0xFFFFFFFF);
+  static const onPrimary = Color(0xFFF8F4E8);
+  static const onSecondary = Color(0xFFF8F4E8);
+  static const onTertiary = Color(0xFFF8F4E8);
+  static const onSurface = Color(0xFF171a0b);
+  static const onBackground = Color(0xFF171a0b);
 
-  static ColorScheme colors = ColorScheme.fromSeed(seedColor: seedColor);
+  static ColorScheme colors = ColorScheme(
+    primary: primary,
+    secondary: secondary,
+    tertiary: tertiary,
+    onPrimary: onPrimary,
+    onSecondary: onSecondary,
+    onTertiary: onTertiary,
+    error: tertiary,
+    onError: onTertiary,
+    surface: background,
+    surfaceContainer: surface,
+    onSurface: onBackground,
+    brightness: Brightness.light,
+  );
 }
